501766a64efdf57d3fd976e8597822063ee91fc6
XVND83 <XVND83@XVND83-Z14G8>
2022-05-28 22:16:08 +0200

    Finished Excersice Level 0

--- Computer_Painter/Source.c
+++ Computer_Painter/Source.c
@@ -3,10 +3,8 @@
 #include <stdio.h>
 #include <conio.h>  
 
-
+int no_rows, row, column, x, user_choice, j;
 // all shapes whill programm will use put into fuction void
-
-/*
 
 void drawRectangle() {
     for (column = 1; column <= no_rows; column++)
@@ -43,28 +41,40 @@ void drawChangingPairs() {
         printf("\n");
     }
 }
-*/
+
 
 int main()
 {
-    int no_rows, row, column, x, user_choice, j;
+    printf("\nHow many characters you would like to use? >>  ");
+    scanf("%d", &no_rows);
 
-    /*
+
     printf("What shape would you like to paint?\n\n1. Rectangle\n2. Triangle\n3. Alternatery changing pairs\n  >>  ");
     scanf("%d",&user_choice);
-    */
-           
-    printf("\nHow many characters you would like to use? >>  ");
-    scanf("%d", no_rows);
 
-    for (row = 1; row <= no_rows; row++)
-    {
-        for (j = 0; j <= no_rows; j++)
-        {
-            row % 2 == 0 ? printf("Xo") : printf("oX");
 
-        }
+    switch (user_choice) {
+
+    case 1:
+        drawRectangle();
         printf("\n");
+
+        break;
+    
+    case 2:
+    drawTriangle();
+    printf("\n");
+
+        break;
+
+    case 3:
+    drawChangingPairs();
+    printf("\n");
+
+        break;
+    
+    default:
+        break;
     }
 
     return 0;
