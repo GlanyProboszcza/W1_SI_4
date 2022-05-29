2e14d1a5eb2249adf06d7c8e2b1496658dd748b5
XVND83 <XVND83@XVND83-Z14G8>
2022-05-29 17:35:59 +0200

    Fixed excersise Level 0 and Level 1 - there were problem with counting columns and rows - changed value form 1 to 0. Added part of excersise Level 2. Added comments

--- Computer_Painter/Source.c
+++ Computer_Painter/Source.c
@@ -11,9 +11,9 @@ int no_rows, row, column, user_choice, j;
 
 // Function to print solid rectangle
 void drawRectangle() {
-    for (column = 1; column <= no_rows; column++) 
+    for (column = 0; column <= no_rows - 1; column++) 
     {
-        for (row = 1; row <= no_rows; row++)
+        for (row = 0; row <= no_rows - 1; row++)
         {
             printf("X ");
         }
@@ -21,25 +21,23 @@ void drawRectangle() {
     }
 }
 
-// Function to print solid rectangle
+// Function to print solid triangle
 void drawTriangle() {
-    for (row = 1; row <= no_rows; row++)
+    for (row = 0; row <= no_rows - 1; row++)
     {
-        for (column = 1; column <= row; column++)
+        for (column = 0; column <= row; column++)
         {
             printf("X ");
         }
         printf("\n");
-
     }
-
 }
 
-// Function to print changing pairs od Xo and oX
+// Function to print rectangle with changing pairs of: Xo and oX
 void drawChangingPairs() {
-    for (row = 1; row <= no_rows; row++)
+    for (row = 0; row <= no_rows - 1; row++)
     {
-        for (j = 0; j <= no_rows; j++) 
+        for (j = 0; j <= no_rows - 1; j++) 
         {
             row % 2 == 0 ? printf("Xo") : printf("oX");
 
@@ -49,18 +47,17 @@ void drawChangingPairs() {
 }
 // Function to print hollow rectangle
 void drawFrame() {
-    for (row = 1; row <= no_rows; row++)
+    for (row = 0; row <= no_rows -1 ; row++)
     {
-        for (column = 1; column <= no_rows; column++)
+        for (column = 0; column <= no_rows - 1; column++)
         {
-            if (row == 1 || row == no_rows || column == 1 || column == no_rows)
+            if (row == 0 || row == no_rows - 1 || column == 0 || column == no_rows - 1)
                 printf("X");
             else
                 printf("o");
         }
         printf("\n");
     }
-
 }
 // Function to print hollow rectangle with diagonal
 void drawX() {
@@ -77,55 +74,111 @@ void drawX() {
         }
         printf("\n");
     }
+}
+// Fuction to print diamond pattern
+void drawDiamond() {
+    int front_space = 0;
+    int back_space = no_rows % 2 == 0 ? 1 : 2;
+    for (row = 0; row < no_rows; row++)
+    {
+        for (column = 0; column < no_rows - back_space; column++)
+        {
+            if (column <= no_rows / 2 - 1 - front_space || column >= no_rows / 2 - 1 + front_space)
+                printf(".");
+            else
+                printf("X");
+        }
+        printf("\n");
+        if (row < no_rows / 2)
+            front_space++;
+        else
+            front_space--;
+    }
+}
+// Fuction to print chess pattern
+void drawChess() {
+
+
+}
+// Function to print "Frame inside frame"
+void drawFrameInsideFrame() {
+
 
 }
 
+
+
+
 int main()
 {
-    printf("\nHow many characters you would like to use? >>  ");
-    scanf("%d", &no_rows);
+    printf("HI! I'M COMPUTER PAINTER! I DRAW REALLY NICE PATTERNS!!\nLet's try!\n");
 
-    
-    printf("What shape would you like to paint?\n\n1. Rectangle\n2. Triangle\n3. Alternatery changing pairs\n4. Rectangle with frame\n5. Rectangle witg diagonals\n  >>  ");
-    scanf("%d",&user_choice);
-    
-    // program will use the switch statment which allows us to execute one code block among many alternatives.
-    switch (user_choice) {
+    do 
+    {
+        printf("\nHow many characters you would like to use? >>  ");
+        scanf("%d", &no_rows);
 
-    case 1:
-        drawRectangle();
-        printf("\n");
 
-        break;
-    
-    case 2:
-    drawTriangle();
-    printf("\n");
+        printf("What shape would you like to paint?\n\n1. Rectangle\n2. Triangle\n3. Alternatery changing pairs\n4. Rectangle with frame\n5. Rectangle with diagonals\n"
+            "6. Diamnod\n7. Chess\n8. Frame inside frame\n  >>  ");
+        scanf("%d", &user_choice);
 
-        break;
+        // program will use the switch statment which allows us to execute one code block among many alternatives.
+        switch (user_choice) {
 
-    case 3:
-    drawChangingPairs();
-    printf("\n");
+        case 1:
+            drawRectangle();
+            printf("\n");
 
-        break;
+            break;
 
-    case 4:
-    drawFrame();
-    printf("\n");
-    
-        break;
+        case 2:
+            drawTriangle();
+            printf("\n");
 
-    case 5:
-        drawX();
-        printf("\n");
+            break;
 
-        break;
-    
-    default:
-        break;
-    }
-    
+        case 3:
+            drawChangingPairs();
+            printf("\n");
+
+            break;
+
+        case 4:
+            drawFrame();
+            printf("\n");
+
+            break;
+
+        case 5:
+            drawX();
+            printf("\n");
+
+            break;
+
+        case 6:
+            drawDiamond();
+            printf("\n");
+
+            break;
+
+        case 7:
+            drawChess();
+            printf("\n");
+
+            break;
+
+        case 8:
+            drawFrameInsideFrame();
+            printf("\n");
+
+            break;
+
+        default:
+            break;
+        }
+
+    } while (1);
 
     return 0;
 
