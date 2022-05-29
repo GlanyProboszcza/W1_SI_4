36ed1df827622efc6114de0fcb98bf0538967c62
XVND83 <XVND83@XVND83-Z14G8>
2022-05-29 14:32:17 +0200

    Finished Excersise Level 1

--- Computer_Painter/Source.c
+++ Computer_Painter/Source.c
@@ -3,11 +3,15 @@
 #include <stdio.h>
 #include <conio.h>  
 
-int no_rows, row, column, x, user_choice, j;
-// all shapes whill programm will use put into fuction void
+// all variables which program will use 
+int no_rows, row, column, user_choice, j;
 
+
+// all functios with specific shapes which main() will use
+
+// Function to print solid rectangle
 void drawRectangle() {
-    for (column = 1; column <= no_rows; column++)
+    for (column = 1; column <= no_rows; column++) 
     {
         for (row = 1; row <= no_rows; row++)
         {
@@ -17,7 +21,8 @@ void drawRectangle() {
     }
 }
 
-void drawTriangle(x) {
+// Function to print solid rectangle
+void drawTriangle() {
     for (row = 1; row <= no_rows; row++)
     {
         for (column = 1; column <= row; column++)
@@ -30,6 +35,7 @@ void drawTriangle(x) {
 
 }
 
+// Function to print changing pairs od Xo and oX
 void drawChangingPairs() {
     for (row = 1; row <= no_rows; row++)
     {
@@ -41,18 +47,49 @@ void drawChangingPairs() {
         printf("\n");
     }
 }
+// Function to print hollow rectangle
+void drawFrame() {
+    for (row = 1; row <= no_rows; row++)
+    {
+        for (column = 1; column <= no_rows; column++)
+        {
+            if (row == 1 || row == no_rows || column == 1 || column == no_rows)
+                printf("X");
+            else
+                printf("o");
+        }
+        printf("\n");
+    }
 
+}
+// Function to print hollow rectangle with diagonal
+void drawX() {
+    for (row = 1; row <= no_rows; row++)
+    {
+        for (column = 1; column <= no_rows; column++)
+        {
+           /* Print X for, first row (row == 1) or last row (row == no_rows) or first column (column == 1) or last column (column == no_rows)
+            * or row equal to column (row == column) or column equal to no_row (column == no_rows - row + 1)*/
+            if(row == 1 || row == no_rows || column == 1 || column == no_rows || row == column || column == (no_rows - row + 1))
+                printf("X");
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
+
+}
 
 int main()
 {
     printf("\nHow many characters you would like to use? >>  ");
     scanf("%d", &no_rows);
 
-
-    printf("What shape would you like to paint?\n\n1. Rectangle\n2. Triangle\n3. Alternatery changing pairs\n  >>  ");
+    
+    printf("What shape would you like to paint?\n\n1. Rectangle\n2. Triangle\n3. Alternatery changing pairs\n4. Rectangle with frame\n5. Rectangle witg diagonals\n  >>  ");
     scanf("%d",&user_choice);
-
-
+    
+    // program will use the switch statment which allows us to execute one code block among many alternatives.
     switch (user_choice) {
 
     case 1:
@@ -72,10 +109,23 @@ int main()
     printf("\n");
 
         break;
+
+    case 4:
+    drawFrame();
+    printf("\n");
+    
+        break;
+
+    case 5:
+        drawX();
+        printf("\n");
+
+        break;
     
     default:
         break;
     }
+    
 
     return 0;
 
